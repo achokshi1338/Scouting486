f149b15082d1f3afaa9f9afe1aa1e824819f39f6
pawnPhilosopher <dvr@fastmail.fm>
2014-10-15 20:26:03 -0400

    fresh start

--- Scouting486/io/github/positronictraining/scoutingapp/Main.java
+++
@@ -1,9 +0,0 @@
-package io.github.positronictraining.scoutingapp;
-
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MainMenu mainMenu = new MainMenu();
-	}
-}
