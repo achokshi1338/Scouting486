03e4af16556d216a7a554595848e993bd33399cb
pawnPhilosopher <dvr@fastmail.fm>
2014-10-17 16:31:48 -0400

    fixed amans commit

--- Scouting486/src/scoutingapp/RoboticsTeam.java
+++ Scouting486/src/scoutingapp/RoboticsTeam.java
@@ -1,19 +1,6 @@
+package scoutingapp;
 
-public class Student {
+public class RoboticsTeam {
 	String name;
-	String points;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPoints() {
-		return points;
-	}
-	public void setPoints(String points) {
-		this.points = points;
-	}
-	
-
+	String playerType;
 }
