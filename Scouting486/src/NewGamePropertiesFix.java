fd02985081189850bb7e73b017629f393417fae1
achokshi1338 <aman.chokshi@gmail.com>
2014-10-17 16:25:46 -0400

    fix
    i fixed periods to points

---
+++ Scouting486/src/NewGamePropertiesFix.java
@@ -0,0 +1,19 @@
+
+public class Student {
+	String name;
+	String points;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPoints() {
+		return points;
+	}
+	public void setPoints(String points) {
+		this.points = points;
+	}
+	
+
+}
