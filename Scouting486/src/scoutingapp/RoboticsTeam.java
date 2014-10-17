cfa922c5b6db11e3af403a091554baf5588e5d46
achokshi1338 <aman.chokshi@gmail.com>
2014-10-17 16:30:08 -0400

    fix
    renamed periods to points

--- Scouting486/src/scoutingapp/RoboticsTeam.java
+++ Scouting486/src/scoutingapp/RoboticsTeam.java
@@ -1,19 +1,19 @@
-package scoutingapp;
 
-public class RoboticsTeam { //I changed this to robotics team and moved it into the scouting app package (for now) to make life easier
+public class Student {
 	String name;
-	String periods; //what do you mean by periods?
-	//TODO: add picture (you dont have to worry about it now, this code is just a reminder for our future selves.
+	String points;
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getPeriods() {
-		return periods;
+	public String getPoints() {
+		return points;
 	}
-	public void setPeriods(String periods) {
-		this.periods = periods;
+	public void setPoints(String points) {
+		this.points = points;
 	}
+	
+
 }
