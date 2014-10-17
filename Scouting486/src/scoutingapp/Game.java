a0d0ca8edb4cfd6290fa52bf58d186f1e881dc18
pawnPhilosopher <dvr@fastmail.fm>
2014-10-17 16:29:24 -0400

    fix to arraylist problem

--- Scouting486/src/scoutingapp/Game.java
+++ Scouting486/src/scoutingapp/Game.java
@@ -6,8 +6,8 @@ public class Game {
 	int year;
 	Point newPointType;
 	String gameName;
-	ArrayList<String> competitions;
-	public ArrayList<Point> pointTypes;
+	ArrayList<String> competitions = new ArrayList<String>();
+	public ArrayList<Point> pointTypes = new ArrayList<Point>();
 	
 	public void createNewPointType(String pointName, int pointValue, String period){
 		newPointType = new Point("high-goal", "autonomous", pointValue);
