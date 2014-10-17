78425829385d12f9e56d4a8254b6952d4a54d332
achokshi1338 <aman.chokshi@gmail.com>
2014-10-16 20:08:00 -0400

    New Game Properties
    Getters and Setters for Name and periods

---
+++ Scouting486/Student.java
@@ -0,0 +1,19 @@
+
+public class Student {
+	String name;
+	String periods;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPeriods() {
+		return periods;
+	}
+	public void setPeriods(String periods) {
+		this.periods = periods;
+	}
+	
+
+}
